07e7a511d02e59a979fb4cfdf83e0a99ac7452c0
ondayary@gmail.com <ondayary@gmail.com>
2022-11-10 14:11:02 +0900

    오류 발생으로 수정 could not obtain connection to query metadata

--- src/main/java/com/mustache/bbs3/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs3/domain/entity/Article.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "article")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
