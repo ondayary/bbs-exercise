97ebc54e5e9c76c21fc797c9838da70ea63b5387
ondayary@gmail.com <ondayary@gmail.com>
2022-11-11 21:07:27 +0900

    [Feat] HospitalController - hospitalList() 추가 병원 전체 리스트 조회 기능

--- src/main/java/com/mustache/bbs3/controller/HospitalController.java
+++ src/main/java/com/mustache/bbs3/controller/HospitalController.java
@@ -1,10 +1,17 @@
 package com.mustache.bbs3.controller;
 
+import com.mustache.bbs3.domain.entity.Hospital;
 import com.mustache.bbs3.domain.repository.HospitalRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/hospitals")
@@ -24,4 +31,12 @@ public class HospitalController {
         return "redirect:/hospitals/list";
     }
 
+    // 병원 전체 리스트 조회 페이지
+    @GetMapping("/list")
+    public String hospitalList(Model model) {
+        log.info("hospitalList 호출");
+        List<Hospital> hospitalList = hospitalRepository.findAll();
+        model.addAttribute("hospitals", hospitalList);
+        return "hospitals/list";
+    }
 }
