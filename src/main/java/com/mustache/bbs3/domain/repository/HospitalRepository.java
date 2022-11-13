8e745c059a759f4f9cb7ee9265d4d1abf5e90257
ondayary@gmail.com <ondayary@gmail.com>
2022-11-14 00:18:17 +0900

    [Refactor] 페이징 기능 추가로 HospitalRepository 수정

--- src/main/java/com/mustache/bbs3/domain/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs3/domain/repository/HospitalRepository.java
@@ -1,7 +1,10 @@
 package com.mustache.bbs3.domain.repository;
 
 import com.mustache.bbs3.domain.entity.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    Page<Hospital> findAll(Pageable pageable);
 }
