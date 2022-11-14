88c685e07402b8b06384e7dcc57afd7a4648b10b
ondayary@gmail.com <ondayary@gmail.com>
2022-11-14 15:12:25 +0900

    Feat(JPQL): Containing, StartsWith, EndingWith 작성

--- src/main/java/com/mustache/bbs3/domain/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs3/domain/repository/HospitalRepository.java
@@ -5,6 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
     Page<Hospital> findAll(Pageable pageable);
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+    List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
+    List<Hospital> findByRoadNameAddressStartsWith(String keyword); // 시작
+    List<Hospital> findByRoadNameAddressEndingWith(String keyword); // 끝남
 }
