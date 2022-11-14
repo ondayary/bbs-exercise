e089f7e66ba82e656a4b5328f8f9bdcd6f995c03
ondayary@gmail.com <ondayary@gmail.com>
2022-11-14 14:36:08 +0900

    Comment(update): 쿼리문의 상세 주석 수정

--- src/main/resources/sql/hospital.sql
+++ src/main/resources/sql/hospital.sql
@@ -1,9 +1,9 @@
--- 경기도 수원시에 있는 피부과 검색
+-- 경기도 수원시에 있는 피부과를 조회하는 쿼리문
 SELECT * FROM likeliondb.nation_wide_hospitals
 where full_address like '%경기도 수원시%'
 and hospital_name like '%피부과%';
 
--- 특정 구의 - 보건진료소, 보건지소, 보건소 모두 찾기
+-- 특정 주소와 업태구분명으로 조회하는 쿼리문
 SELECT business_type_name, hospital_name, road_name_address
 FROM likeliondb.nation_wide_hospitals
 where business_type_name in ('보건소', '보건지소');
