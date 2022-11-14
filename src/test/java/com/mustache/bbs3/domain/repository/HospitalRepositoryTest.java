ee44cd18baffd9b1ed99af838a9389a7510cd408
ondayary@gmail.com <ondayary@gmail.com>
2022-11-15 02:33:40 +0900

    Test: "추가 업태구분명 조회 메서드" BusinessTypeName을 찾기 위해 findByBusinessTypeNameIn()를 생성 후 테스트 코드 작성

--- src/test/java/com/mustache/bbs3/domain/repository/HospitalRepositoryTest.java
+++ src/test/java/com/mustache/bbs3/domain/repository/HospitalRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.mustache.bbs3.domain.repository;
 
 import com.mustache.bbs3.domain.entity.Hospital;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,6 +17,19 @@ class HospitalRepositoryTest {
 
     @Autowired
     HospitalRepository hospitalRepository;
+
+    @Test
+    @DisplayName("업태구분명으로 찾기")
+    void findByBusinessTypeNameIn() {
+        List<String> inClues = new ArrayList<>();
+        inClues.add("보건소");
+        inClues.add("보건지소");
+        inClues.add("보건진료소");
+        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
+        for (var hospital : hospitals) {
+            System.out.printf("%s, %s\n", hospital.getHospitalName(), hospital.getBusinessTypeName());
+        }
+    }
 
     // 먼저 아이디 한개를 뽑아 테스트해보자.
     @Test
