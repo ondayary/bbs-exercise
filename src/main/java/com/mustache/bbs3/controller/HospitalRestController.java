0ca3a9fb6b3f8ad1c508964d3f5edd88511d96dc
ondayary@gmail.com <ondayary@gmail.com>
2022-11-15 14:46:06 +0900

    Feat: "적용 레이어드 아키텍쳐" 1. Hospital.java - businessStatusCode 추가 2. HospitalService.java - 비즈니스로직 추가 3. HospitalRestController.java가 HospitalService를 의존하게 변경

--- src/main/java/com/mustache/bbs3/controller/HospitalRestController.java
+++ src/main/java/com/mustache/bbs3/controller/HospitalRestController.java
@@ -3,6 +3,7 @@ package com.mustache.bbs3.controller;
 import com.mustache.bbs3.domain.dto.HospitalResponse;
 import com.mustache.bbs3.domain.entity.Hospital;
 import com.mustache.bbs3.domain.repository.HospitalRepository;
+import com.mustache.bbs3.service.HospitalService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,17 +16,16 @@ import java.util.Optional;
 @RequestMapping("/api/v1/hospitals")
 public class HospitalRestController {
 
-    private final HospitalRepository hospitalRepository;
+    private final HospitalService hospitalService;
 
-    public HospitalRestController(HospitalRepository hospitalRepository) {
-        this.hospitalRepository = hospitalRepository;
+    public HospitalRestController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<HospitalResponse> get(@PathVariable Integer id) { // ResponseEntity도 DTO타입
-        Optional<Hospital> hospital = hospitalRepository.findById(id); // Entity
-        HospitalResponse hospitalResponse = Hospital.of(hospital.get()); // DTO
-        return ResponseEntity.ok().body(hospitalResponse); // return은 DTO로
+        HospitalResponse hospitalResponse = hospitalService.getHospital(id); // DTO
+        return ResponseEntity.ok().body(hospitalResponse); // Return은 DTO로
     }
 }
 
---
+++ src/main/java/com/mustache/bbs3/service/HospitalService.java
@@ -0,0 +1,32 @@
+package com.mustache.bbs3.service;
+
+import com.mustache.bbs3.domain.dto.HospitalResponse;
+import com.mustache.bbs3.domain.entity.Hospital;
+import com.mustache.bbs3.domain.repository.HospitalRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class HospitalService {
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalService(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+    public HospitalResponse getHospital(Integer id) {
+        Optional<Hospital> optHospital = hospitalRepository.findById(id); // Entity
+        Hospital hospital = optHospital.get();
+        HospitalResponse hospitalResponse = Hospital.of(hospital); // DTO
+        if (hospital.getBusinessStatusCode() == 13) {
+            hospitalResponse.setBusinessStatusName("영업중");
+        } else if (hospital.getBusinessStatusCode() == 3) {
+            hospitalResponse.setBusinessStatusName("폐업");
+        } else {
+            hospitalResponse.setBusinessStatusName(String.valueOf(hospital.getBusinessStatusCode()));
+        }
+        return hospitalResponse;
+    }
+}
+
