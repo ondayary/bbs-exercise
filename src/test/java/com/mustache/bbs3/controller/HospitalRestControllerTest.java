aa517b7ccd8e54d10e91f4634fac70ec227ba065
ondayary@gmail.com <ondayary@gmail.com>
2022-11-15 14:51:01 +0900

    Test: "추가 RestContollerTest 클래스" @WebMvcTest: Controller를 테스트하게 해주는 기능

---
+++ src/test/java/com/mustache/bbs3/controller/HospitalRestControllerTest.java
@@ -0,0 +1,8 @@
+package com.mustache.bbs3.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(HospitalRestControllerTest.class) // Controller를 테스트하게 해주는 기능
+class HospitalRestControllerTest {
+
+}
