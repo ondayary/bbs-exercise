0e7a792032830b6ef0b1fc25ed1a09159c0bdbf2
ondayary@gmail.com <ondayary@gmail.com>
2022-11-16 11:42:21 +0900

    Reactor: "수정 Article을 ArticleDto로 바꾸는 로직"

--- src/main/java/com/mustache/bbs3/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs3/domain/entity/Article.java
@@ -1,5 +1,6 @@
 package com.mustache.bbs3.domain.entity;
 
+import com.mustache.bbs3.domain.dto.ArticleDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,22 +8,27 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "article")
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
+@Table(name = "article")
 public class Article {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String title;
     private String content;
 
-    public Article(String title, String content) {
+    public static ArticleDto of(Article article) {
+        return new ArticleDto(article.getId(), article.getTitle(), article.getContent());
+    }
+
+    /*public Article(String title, String content) {
         this.title = title;
         this.content = content;
-    }
+    }*/
 }
 
 
